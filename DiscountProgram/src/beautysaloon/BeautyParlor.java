270bec17e458138ee2e52d8d725809cb2fda37e6
shashi h <shashi.haricharan@yahoo.in>
2019-08-05 13:20:00 +0530

    Added more code

---
+++ DiscountProgram/src/beautysaloon/BeautyParlor.java
@@ -0,0 +1,53 @@
+package beautysaloon;
+
+import java.util.ArrayList;
+
+public class BeautyParlor {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Customer> customers=new ArrayList<Customer>();
+		ArrayList<Visit> visits=new ArrayList<Visit>();
+		Customer customer1=new Customer("Rose");
+		customer1.setMember(true);
+		customer1.setMemberType("PREMIUM");
+		Visit visit1=new Visit(customer1.getName());
+		visit1.setServiceExpense(1000.30);
+		visit1.setProductExpense(2000.50);
+		Customer customer2=new Customer("Emily");
+		customer2.setMember(false);
+		customer2.setMemberType("GOLD");
+		Visit visit2=new Visit(customer2.getName());
+		visit2.setServiceExpense(3000);
+		visit2.setProductExpense(2000);
+		customers.add(customer1);
+		customers.add(customer2);
+		visits.add(visit1);
+		visits.add(visit2);
+		for(Customer cust:customers){
+			System.out.println(cust.toString());
+		for(Visit visit:visits){
+			double totalBill=0;
+			if(cust.isMember()==true){
+			double service=0;double product=0;
+			double serviceExpense=visit.getServiceExpense()*DiscountRate.getServiceDiscountRate(cust.getMemberType());
+			double productExpense=visit.getProductExpense()*DiscountRate.getProductDiscountRate(cust.getMemberType());
+			service=visit.getServiceExpense()-serviceExpense;
+			product=visit.getProductExpense()-productExpense;
+			totalBill=service+product;
+			System.out.println("Total Bill of customer"+cust.getName()+ "is :"+totalBill);
+			}
+			else{
+			
+			double service=0;double product=0;
+			double serviceExpense=visit.getServiceExpense()*0.1;
+			double productExpense=visit.getProductExpense()*0.1;
+			service=visit.getServiceExpense()-serviceExpense;
+			product=visit.getProductExpense()-productExpense;
+			totalBill=service+product;
+			System.out.println("Total Bill of customer"+cust.getName()+ "is :"+totalBill);
+		}
+	}
+	}
+	}
+}
---
+++ DiscountProgram/src/beautysaloon/DiscountRate.java
@@ -0,0 +1,34 @@
+package beautysaloon;
+
+public class DiscountRate {
+
+	private static double serviceDiscountPremium=0.2;
+	private static double serviceDiscountGold=0.15;
+	private static double serviceDiscountSilver=0.1;
+	private static double productDiscountPremium=0.1;
+	private static double productDiscountGold=0.1;
+	private static double productDiscountSilver=0.1;
+	
+	public static double getServiceDiscountRate(String type){
+		double serviceDiscount=0;
+		if(type.equals("PREMIUM"))
+			serviceDiscount= serviceDiscountPremium;
+		else if(type.equals("GOLD"))
+			serviceDiscount= serviceDiscountGold;
+		else if(type.equals("SILVER"))
+			serviceDiscount= serviceDiscountSilver;
+		return serviceDiscount;
+	}
+	
+	public static  double getProductDiscountRate(String type){
+		double productDiscount=0;
+		if(type.equals("PREMIUM"))
+			productDiscount= productDiscountPremium;
+		else if(type.equals("GOLD"))
+			productDiscount= productDiscountGold;
+		else if(type.equals("SILVER"))
+			productDiscount= productDiscountSilver;
+		return productDiscount;
+	}
+	
+}
